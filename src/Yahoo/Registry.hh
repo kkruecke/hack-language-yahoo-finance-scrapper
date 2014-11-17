1a5144059673cf53c88fb3bc983c2bf36aa33c68
Kurt Krueckeberg <kurtk@pobox.com>
2014-11-17 12:44:06 -0600

    changes

--- src/Yahoo/Registry.hh
+++ src/Yahoo/Registry.hh
@@ -3,31 +3,27 @@ namespace Yahoo;
 
 class Registry {
 
-  static private  \ArrayObject $arrayObject;
+  static private  \ArrayObject $arrayObject; 
 
   public function __construct()
   {
      /* TODO: Read ini file */
-	  //if (self::$arrayObject == null) {
-	  //
      if (!isset(self::$arrayObject)) {	  
 	  
-        self::$arrayObject = new \ArrayObject(); // TODO: What should I pass ctor?
+        self::$arrayObject = new \ArrayObject(); 
      }
   }
 
   public static function set($property, $value) : void
   {
-      //self::$arrayObject[$property] = $value;
-      self::$arrayObject->property = $value;
+      self::$arrayObject[$property] = $value;
   }
 
   public static function get($key) : mixed
   {
        if (self::$arrayObject->offSetExists($key)) {
 
-	  return self::$arrayObject->key;
-	  //return self::$arrayObject[$key];
+	  return self::$arrayObject[$key];
 
        } else {
 
@@ -35,4 +31,19 @@ class Registry {
 
        }
   }
+}
+
+try {
+  $r1 = new Registry();
+  $r2 = new Registry();
+  $r1::set('key', 'some-value');
+  $x = $r1::get('key');
+  
+  var_dump($x);
+  echo "\n";
+  print_r($r1);
+} catch (\Exception $e) {
+
+	echo $e->getMessage(). "\n============\n\n";
+	echo $e->getTraceAsString() . "\n";
 }
