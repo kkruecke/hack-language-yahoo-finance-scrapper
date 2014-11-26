9e4d634944aead32c84609e7560f218dc558b48e
Kurt Krueckeberg <kurtk@pobox.com>
2014-11-26 15:37:06 -0500

    changes

--- src/Yahoo/CustomStockFilterIterator.hh
+++ src/Yahoo/CustomStockFilterIterator.hh
@@ -6,11 +6,23 @@ class CustomStockFilterIterator extends \FilterIterator {
         // Only accept strings with a length of 10 and greater
         $row = parent::current();
 
-	/* TODO
-	 * Implement Criteria: 
-	 *  All columns non-empty
-	 *  US Stocks only
+	/* 
+	 * Filter criteria: Only accept rows with all non-empty columns
 	 */  
+	foreach($row as $columnn_num => $column_text) {
+                /*
+		 * All column must be non-empty
+		 */  
+		if (strlen($column_text) == 0) {
+			return false;
+		}
+	}
+        /*
+	 * Criteria for US Stock below
+	 */ 
+        $stock_length = strlen($row[1]);
+
+        return (($stock_length > 1 && $stock_length < 5) && ( strpos($row[1], '.') === FALSE)) ? true : false;
 
     }
 }
