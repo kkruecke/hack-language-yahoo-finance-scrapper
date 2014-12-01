8f505d6fff0c53d5f9816fdfcd2dd1b97efaf52e
Kurt Krueckeberg <kurtk@pobox.com>
2014-12-01 18:19:53 -0500

    changes

--- test.hh
+++ test.hh
@@ -31,75 +31,9 @@ boot_strap();
        return;
   }
 
-  $start_date = \DateTime::createFromFormat('m/d/Y', $argv[1]); 
+  $str =  Registry::registry('xpath-query');
 
-  $date_period = build_date_period($start_date, (int) $argv[2]);
+  $debug = 10;
 
-  /*
-   * CSVYahooFormatter determines the format of the output, the rows of the CSV file.
-   */  
-  $file_name = $start_date->format('jmY') . "-plus-" . $argv[2] . ".csv";
-    
- 
-  $csv_writer = new CSVWriter($file_name, new CSVYahooFormatter($start_date));
+  echo $str;
 
-  print_r($date_period);
-  return;
-
-  // Start main loop
-  foreach ($date_period as $date_time) {
-
-      echo "go inside foreach\n";
-      return;
-
-      $url = make_url($date_time); // Build yyyymmdd.html name
-
-      $pretty_date = $date_time->format("m-d-Y"); // User-friendly date format
-      
-      if (validate_url_existence($url)) {
-          
-           echo 'Skipping date ' . $pretty_date . " there is no webpage $url ...\n";               
-           continue;    
-      }
-      
-      try {
-          
-	  $table = new YahooTable($url, Registry::registry('xpath-query'));
-
-	  $max_rows = $table->rowCount(); // first row is 0, last is $max_rows - 1
-	     
-	  // We skip the first two rows, the table description and column headers, and the last row which has no financial data
-	  $limitIter = new \LimitIterator($table->getIterator(), 2, $max_rows - 2); // TODO: Check whether it is "- 2" or "- 1"?
-
-	  /*
-	   * The filter iterator should include all the filters of the original code:
-	   *   1. no column may be blank
-	   *   2. only US Stocks are selected
-	   *   3. ? any other filters
-	   */   
-	  $filterIter = new \CustomStockFilterIterator($limitIter);
-          /*
-	   * Alternately, a custom callback filter iterator could be used: 
-	   *   $callbackFilterIter = new \CallbackFilterIterator($rowExtractorIter, 'isUSStock_callback');
-	   */ 
-     
-          foreach($filterIter as $us_stock_row) {
-
-               $csv_writer->writeLine($us_stock_data); 
-	  }
-
-	  echo "Date $pretty_date processed\n";
-
-  
-      } catch(Exception $e) {
-          
-          echo $e->getMessage() . "\n";
-          return;
-      }
-  }
-
-  $line_count = $csv_writer->getLineCount();
-  
-  echo  $csv_writer->getFileName() . " has been created. It contains $line_count US stocks entries.\n";
-    
-  return;
